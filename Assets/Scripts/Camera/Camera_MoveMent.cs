39170cee175a6acc6977f323729fc17b5b832fb2
GGueMMue <daehan121993@gmail.com>
2024-10-16 18:19:15 +0900

    카메라 Movement 수정 필요
    기괴하고 불편함.
    
    원인은 OnTrigger 함수들이라고 생각됨.

---
+++ Assets/Scripts/Camera/Camera_MoveMent.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Camera_MoveMent : MonoBehaviour
+{
+    Transform                               playerTr;
+    BoxCollider                             MovementCheckerBoxColider;
+
+
+    public Vector3                                 startPos;
+    public Vector3                                 newPos;
+
+    public bool                             checker = false;
+
+    public float                            fixed_Y = 10f;
+
+    public  float value = 0.125f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTr = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        MovementCheckerBoxColider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (checker)
+        {
+            Vector3 desiredPos = new Vector3(playerTr.position.x, fixed_Y, playerTr.position.z);
+            // XZ 축에 대해서만 부드럽게 보간 (Y는 고정)
+            newPos = Vector3.Lerp(transform.position, desiredPos, 0.5f);
+
+            // 카메라의 위치를 업데이트 (Y축 고정)
+            transform.position = new Vector3(newPos.x, fixed_Y, newPos.z);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            checker = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        startPos = this.gameObject.transform.position;
+        if(other.gameObject.CompareTag("Player"))
+        {
+            checker = true;
+        }
+    }
+}
