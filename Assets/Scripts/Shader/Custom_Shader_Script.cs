e61fcae4f0e2f371e0331e766113f93fc1afd158
GGueMMue <daehan121993@gmail.com>
2024-10-28 16:11:22 +0900

    URP로 변경 + 쉐이더 추가

--- Assets/Scripts/Shader/Custom_Shader_Script.cs
+++ Assets/Scripts/Shader/Custom_Shader_Script.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Custom_Shader_Script : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public Shader shader;
     void Start()
     {
-        
+        Renderer[] renderers = FindObjectsOfType<Renderer>();
+
+        foreach (Renderer renderer in renderers)
+        {
+            foreach(Material material in renderer.materials)
+            {
+                if (renderer.gameObject.CompareTag("Crosshair")) continue;
+
+                material.shader = shader;
+            }
+        }
     }
 
     // Update is called once per frame
